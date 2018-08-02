846bc1d2d1e9c3ca5870352a5a56a97ab36d62e5
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-08-01 21:24:55 -0400

    adding in some JS

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-let ability1 = document.getElementById('ability1').innerHTML;
-let ability2 = document.getElementById('ability2').innerHTML;
-let hpResult = document.getElementById('hpResult').innerHTML;
-let atkResult = document.getElementById('atkResult').innerHTML;
-let defResult = document.getElementById('defResult').innerHTML;
-let type1 = document.getElementById('type1').innerHTML;
-let type2 = document.getElementById('type2').innerHTML;
+let ability1 = document.getElementById('ability1');
+let ability2 = document.getElementById('ability2');
+let hpResult = document.getElementById('hpResult');
+let atkResult = document.getElementById('atkResult');
+let defResult = document.getElementById('defResult');
+let type1 = document.getElementById('type1');
+let type2 = document.getElementById('type2');
 
 function test() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       let data = JSON.parse(this.responseText);
-      console.log(data.stats[5].base_stat);
+      console.log(data.stats[3].base_stat)
+      // hpResult.innerHTML = data.stats[5].base_stat;
+      // atkResult.innerHTML = data.stats[4].base_stat;
+      // defResult.innerHTML = data.stats[3].base_stat;
     }
   };
   xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/7/", true);
   xhttp.send();
+}
+
+function clearSelection() {
+  console.log('button is working')
+  hpResult.innerHTML = '';
+  atkResult.innerHTML = '';
+  defResult.innerHTML = '';
 }
