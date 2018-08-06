e48566007cc71678b5ed104f85a9321cf1b17ea7
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-08-06 16:48:56 -0400

    up to 65 pokemon

--- main.js
+++ main.js
@@ -8,41 +8,54 @@ let type2 = document.getElementById('type2');
 let image = document.getElementById('pokeImage');
 let input = document.getElementById('text');
 
-
+// function checkType(){
+//   if(type1.innerHTML === 'grass' ){
+//     type1.style.color = 'green';
+//   }
+//   console.log(type1.innerHTML);
+// }
 
 function getPokemon() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       let data = JSON.parse(this.responseText);
+      
       for(let i = 0; i < data.length; i++){
-        if(data[i].name === input.value.toLowerCase()){
-          // console.log(data[i].name)
+        if(data[i].name == input.value.toLowerCase()){
           hpResult.innerHTML = data[i].stats[0].hp;
           atkResult.innerHTML = data[i].stats[1].atk;
           defResult.innerHTML = data[i].stats[2].def;
           ability1.innerHTML = data[i].abilities[0];
           ability2.innerHTML = data[i].abilities[1];
           image.src = data[i].image_url;
-
-          for(let j = 0; j < data[i].types; i++){
-            console.log(data[i].types[j])
-            if(data[i].types[j] === 'water'){
-              type1.style.color = 'blue';
+          
+          for(let j = 0; j < data[i].types.length; j++){
+            // if(data[i].types[j] === 'grass'){
+            //   type1.style.color = 'green';
+            checkType();
               type1.innerHTML = data[i].types[j];
-            } if(data[i].types[j] === 'poison'){
-              type2.style.color = 'purple';
               type2.innerHTML = data[i].types[j];
-            }
+            // } 
+            // if(data[i].types[j] === 'poison'){
+            //   type2.style.color = 'purple';
+            //   type2.innerHTML = data[i].types[j];
+            // }
           }
           
         }
       }
-      console.log(input.value.toLowerCase());
     }
   };
   xhttp.open("GET", "https://raw.githubusercontent.com/ChrisMFernandes/pokedex/master/pokemon.json", true);
   xhttp.send();
+}
+
+function checkType(){
+  if(type1.innerHTML === 'grass' ){
+    type1.style.color = 'green';
+  }
+  console.log(type1.innerHTML);
 }
 
 function clearSelection() {
